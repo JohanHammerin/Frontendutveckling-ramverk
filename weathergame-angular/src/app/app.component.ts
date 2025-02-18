15aa702deb8f9d3c7c1de794b32ce7bbaa959087
johanhammerin <johan04hammerin@gmail.com>
2025-02-18 12:03:49 +0100

    first commit

--- weathergame-angular/src/app/app.component.ts
+++ weathergame-angular/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
